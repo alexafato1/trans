1672216b1a331ccd9b652bf0bf659935fed05977
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-10-15 21:52:56 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation ,Trans} from 'react-i18next';
+import i18next from 'i18next';
+import imageEng from './img/eng.jpg'
+import imageRu from './img/ru.jpg'
+import imageUa from './img/ua.jpg'
+//Компонент Trans - лучший способ перевести дерево JSX за один перевод. 
+//Это позволя например. легко переводить текст, содержащий компонент ссылки или форматирование, подобное .<strong>
+
+
 
 function App() {
+  const { t, i18n } = useTranslation();
+  function handleClick(lang){
+    i18next.changeLanguage(lang)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='App'>
+         <header>
+      
+      <h1>{t('Thanks.1')}</h1>
+      <h2>{t('Why.1')}</h2>
+    </header>
+    <img src={localStorage.getItem('i18nextLng')==='en'
+      ?imageEng : localStorage.getItem('i18nextLng')==='ru'
+      ?imageRu : imageUa}alt='img'/>
+    <nav>
+      <button onClick={()=>handleClick('en')}>
+       English
+      </button>
+      <button onClick={()=>handleClick('ru')}>
+       Russian
+      </button>
+      <button onClick={()=>handleClick('ua')}>
+       Urainian
+      </button>
+    </nav>
+   
     </div>
+
   );
 }
 
---
+++ src/i18n/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+ 
+// для передачи lng и переводов при инициализации
+const Languages = ['en', 'ru', 'ua']
+
+i18n
+   // загружаем перевод, используя http -> see / public / locales (т.е. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales) 
+   // узнать больше: https://github.com/i18next/i18next-http-backend
+  .use(Backend)
+  // определяем язык пользователя 
+  // узнать больше: https://github.com/i18next/i18next-browser-languageDetector . использовать ( LanguageDetector )
+  .use(LanguageDetector)
+  // передаем экземпляр i18n в response-i18next.
+  .use(initReactI18next)
+  // инициализируем i18next 
+  // для всех опций читайте: https://www.i18next.com/overview/configuration-options
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    whitelist: Languages
+    
+  });
+
+
+export default i18n;
